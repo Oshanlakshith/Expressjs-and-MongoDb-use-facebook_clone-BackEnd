85f8aa785f9ad59e8d6f3b001fee9ccf719b5405
Oshanlakshith <oshanlakshitha146@gmail.com>
2022-08-29 12:23:57 +0530

    image uploading,Postman Collection and Compleate p

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app=express()
 const bodyParser= require('body-parser')
 const multer = require('multer');
 const port=4000
-
+app.use(bodyParser.urlencoded({extended: true}))
 
 app.use(express.json())
 
@@ -14,29 +14,43 @@ var Storage = multer.diskStorage({
       cb(null, 'images')
     },
     filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + Date.now())
+      cb(null, file.fieldname + '-' + Date.now()+".jpg")
     }
 })
 
 var upload = multer({ storage: Storage }).array('image', 12)
 console.log(upload);
 
-//route
-app.post('/', (req, res) => {
-    
-});
-
-app.post('/upload', (req, res) => {
+app.post('/upload',(req, res) => {
     upload(req, res , err => {
-        if (err) {
-            res.send('somthing went wrong');
+        try {
+            const response=upload.save()
+            res.json(response)
+        } catch (error) {
+            res.send(error)
         }
-        res.send('file uploaded successfully');
     });
 });
 
+app.get('/upload',async(req,res)=>{
+   try {
+    const image=await upload.find()
+    res.json(image)
+   } catch (error) {
+    res.send("Err"+error)
+   }
+})
 
-const url='mongodb://localhost/fbclone'
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const url = "mongodb+srv://facebookClone:00cjz1rZGpjc8Dct@cluster0.zglor4l.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+//const url='mongodb://localhost/fbclone'
 mongoose.connect(url,{useNewUrlParser:true})
 const con=mongoose.connection
 
